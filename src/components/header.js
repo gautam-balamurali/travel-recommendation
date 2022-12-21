f771fad58b0214d28cd994aa527b5df5f264225b
Gautam Balamurali <gtmbalamurali@gmail.com>
2022-12-21 15:06:16 +0530

    feat: logic and style implementation

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import React from "react";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Travel Recommendation App</h1>
+    </header>
+  );
+}
+export default Header;
